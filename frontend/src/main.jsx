68340be61dd214c05131d93f0990a624c0e7771c
Ubuntu <ubuntu@ip-172-31-30-121.ec2.internal>
2025-11-19 04:39:14 +0000

    Initial working version of mini-twitter full stack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,23 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const rootEl = document.getElementById("root");
+
+function hideSplash() {
+  const s = document.getElementById("splash");
+  if (!s) return;
+  s.classList.add("splash-hidden");
+  setTimeout(()=> s.remove(), 420);
+}
+
+if (rootEl) {
+  createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  setTimeout(hideSplash, 80);
+}
+
